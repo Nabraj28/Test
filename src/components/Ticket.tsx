7f824d1e77481732ab34e990a2f80ea4652dc84a
nabraj28 <nabrajpoudel.28@gmail.com>
2024-02-07 20:21:22 +0545

    Finalizing project

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -6,7 +6,6 @@ const Ticket = () => {
     return (
         <div className='ticketmain' >
             <p className='ticketselect' >Select Ticket</p>
-
             <TicketContainer />
         </div>
     )
