23a420c563e722134a07a303f4e1d9f1348922b4
nabraj28 <nabrajpoudel.28@gmail.com>
2024-02-07 20:37:07 +0545

    Finalizing project

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,7 +4,7 @@ import '../../styles/Gallerystyles.css'
 export const Gallery = () => {
     const data = [
         { id: 1, src: "/mask.jpg" },
-        { id: 2, src: "https://s3-alpha-sig.figma.com/img/a77a/8245/001a1ff9ecf70ecd5064fb1fa0e61dbc?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=THViVaytzuXga~0uMtYLcWVIjd2uriUfiaNj0p6PleUtE8xFGOeHxJrDp1-TWjtDKlnp5u2gMnzORInW7Im3Oys38IDVshaM--bUIdFexaJPlCsFpqV0QRYNXlaqky3mgOoUzQKcpqAmuEoNePJLIg~zPm4yXDX4HyA1ZErnavtDgIhaR5CLdUgPzPzJmw2nehrA4YnkHa3F3lTKVTvZkpUbfr6Da57geOrgwAa9oGFa6Q1K~jpNSg4oKTcmMSApiLke7bndx9XE3fqKDdjes-jkGyv0sPeqq6bLNnnAPU7ZoZvkrZxnxL1X1wthU1JmH5R8gawm4XpIdPt6CMBSyQ__" },
+        { id: 2, src: "/bh.jpg" },
         { id: 3, src: "/ts.jpg" },
     ]
     return (
