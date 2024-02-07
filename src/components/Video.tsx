01fb4aefa892d247920531934ff5a085f9f03790
nabraj28 <nabrajpoudel.28@gmail.com>
2024-02-07 21:11:57 +0545

    Finalizing project

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,7 +6,13 @@ export const Video = () => {
 
     return (
         <div>
-
+            <iframe
+                className="if"
+                src="https://www.youtube.com/embed/qdk01wkzKzo?si=gNs3qD35NpmaBXR8"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+            ></iframe>
         </div>
     )
 }
